351975be7bd09a9f108fafe63916391e3c2ec694
Eleanor Broadway <eleanor@EPCC-MBP-P12-245.local>
2026-03-06 11:30:07 +0000

    initial commit

---
+++ apps/OpenFOAM/ARCHER2-OpenFOAM-v2512/site/modules.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Modules required at build time
+
+module load PrgEnv-gnu
+module load gcc/11.2.0
+module load mkl
+module load cray-fftw
+
+# Modules required at compile time and at run time
